5b085babf75343320933877e398e9680b9a6b1d7
toan <bachmatoan2910@gmail.com>
2023-07-01 09:46:39 -0400

    Lorem Ipsum - Complete

--- 08-lorem-ipsum/starter/src/App.jsx
+++ 08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,40 @@
+import { useState } from "react";
+import data from "./data";
+import { nanoid } from "nanoid";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+  function handleSubmit(event) {
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+    event.preventDefault();
+  }
+  return (
+    <section className="section-center" onSubmit={handleSubmit}>
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount"></label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(event) => setCount(event.target.value)}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
