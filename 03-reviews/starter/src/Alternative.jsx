5cf4bb4b4286030c961c3e75226d90222d69e9f6
toan <bachmatoan2910@gmail.com>
2023-07-06 19:43:09 -0400

    Reviews - Revised

--- 03-reviews/starter/src/Alternative.jsx
+++ 03-reviews/starter/src/Alternative.jsx
@@ -1,48 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { AiFillRightCircle, AiFillLeftCircle } from "react-icons/ai";
+import { FaQuoteRight } from "react-icons/fa";
+
 import people from "./data";
-import {
-  FaChevronCircleLeft,
-  FaChevronCircleRight,
-  FaQuoteRight,
-} from "react-icons/fa";
 
 const App = () => {
   const [index, setIndex] = useState(0);
-  const { name, job, image, text } = people[index];
-  console.log(name);
-  function checkNumber(number) {
-    if (number > people.length - 1) {
-      return 0;
-    } else if (number < 0) {
-      return people.length - 1;
-    }
-    return number;
-  }
 
-  function nextPerson() {
-    setIndex((prevValue) => {
-      let newIndex = prevValue + 1;
-      newIndex = checkNumber(newIndex);
-      return newIndex;
+  const { id, name, job, image, text } = people[index];
+
+  function setPrevious() {
+    setIndex((index) => {
+      if (index === 0) {
+        index = people.length;
+      }
+
+      let newNumber =  (index - 1) % people.length;
+      return newNumber;
     });
   }
 
-  function previousPerson() {
-    setIndex((prevValue) => {
-      let newIndex = prevValue - 1;
-      newIndex = checkNumber(newIndex);
-      return newIndex;
+  function setNext() {
+    setIndex((index) => {
+      return (index + 1) % people.length;
     });
   }
 
-  function randomPerson() {
+  const randomPerson = () => {
     let randomNumber = Math.floor(Math.random() * people.length);
-    console.log(randomNumber);
-    if(randomNumber === index) {
-      randomNumber = index + 1;
-    }
-    setIndex(checkNumber(randomNumber));
-  }
+    setIndex(randomNumber % people.length);
+  };
+
   return (
     <main>
       <article className="review">
@@ -55,15 +43,21 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+
         <div className="btn-container">
-          <button className="prev-btn" onClick={previousPerson}>
-            <FaChevronCircleLeft />
+          <button className="prev-btn" onClick={() => setPrevious()}>
+            <AiFillLeftCircle />
           </button>
-          <button className="next-btn" onClick={nextPerson}>
-            <FaChevronCircleRight />
+          <button className="next-btn" onClick={() => setNext()}>
+            <AiFillRightCircle />
           </button>
         </div>
-        <button className="btn btn-hipster" onClick={randomPerson}>
+        <button
+          className="btn btn-hipster"
+          onClick={() => {
+            randomPerson();
+          }}
+        >
           surprise me
         </button>
       </article>
--- 03-reviews/starter/src/App.jsx
+++ 03-reviews/starter/src/App.jsx
@@ -1,46 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { AiFillRightCircle, AiFillLeftCircle } from "react-icons/ai";
+import { FaQuoteRight } from "react-icons/fa";
+
 import people from "./data";
-import {
-  FaChevronCircleLeft,
-  FaChevronCircleRight,
-  FaQuoteRight,
-} from "react-icons/fa";
 
 const App = () => {
   const [index, setIndex] = useState(0);
-  const { name, job, image, text } = people[index];
-  console.log(name);
+
+  const { id, name, job, image, text } = people[index];
+
   function checkNumber(number) {
-    if (number > people.length - 1) {
-      return 0;
-    } else if (number < 0) {
+    if (number < 0) {
       return people.length - 1;
+    } else if (number > people.length - 1) {
+      return 0;
+    } else {
+      return number;
     }
-    return number;
   }
 
-  function nextPerson() {
-    setIndex((prevValue) => {
-      let newIndex = (prevValue + 1) % people.length;
-      return newIndex;
+  function setPrevious() {
+    setIndex((index) => {
+      return checkNumber(index - 1);
     });
   }
 
-  function previousPerson() {
-    setIndex((prevValue) => {
-      let newIndex = (prevValue - 1 % people.length);
-      return newIndex;
+  function setNext() {
+    setIndex((index) => {
+      return checkNumber(index + 1);
     });
   }
 
-  function randomPerson() {
+  const randomPerson = () => {
     let randomNumber = Math.floor(Math.random() * people.length);
-    console.log(randomNumber);
-    if(randomNumber === index) {
-      randomNumber = index + 1;
-    }
     setIndex(checkNumber(randomNumber));
-  }
+  };
+
   return (
     <main>
       <article className="review">
@@ -53,17 +48,16 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+
         <div className="btn-container">
-          <button className="prev-btn" onClick={previousPerson}>
-            <FaChevronCircleLeft />
+          <button className="prev-btn" onClick={() => setPrevious()}>
+            <AiFillLeftCircle />
           </button>
-          <button className="next-btn" onClick={nextPerson}>
-            <FaChevronCircleRight />
+          <button className="next-btn" onClick={() => setNext()}>
+            <AiFillRightCircle />
           </button>
         </div>
-        <button className="btn btn-hipster" onClick={randomPerson}>
-          surprise me
-        </button>
+        <button className="btn btn-hipster" onClick={() => {randomPerson()}}>surprise me</button>
       </article>
     </main>
   );
